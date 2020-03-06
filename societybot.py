3cce99d88d66f1b85fa82a1c09832ab8822337aa
Paulo Augusto <paulo.augusto.viana@usp.br>
2020-03-06 01:30:58 -0300

    Refactoring

--- societybot.py
+++ societybot.py
@@ -2,84 +2,114 @@ import numpy as np, optparse, random, time, schedule
 from PIL import Image, ImageDraw, ImageFont
 import markovify
 import facebook
+import json
+from collections import namedtuple
 
-def makeText():
+def load_default_config_():
+    with open("./config.json", "r") as config_file:
+        config = json.load(config_file)
+    return config
+
+config_tuple = namedtuple("config", [*load_default_config_().keys()])
+
+def load_default_config():
+    with open("./config.json", "r") as config_file:
+        config = json.load(config_file)
+    return config_tuple(**config)
+
+def get_config(key):
+    config = load_default_config()._asdict()
+    return config[key]
+
+def makeText(quotes_path, state_size=2, sequence_length=50):
     # Get raw text as string.
-    with open(r"C:\Users\Administrator\Desktop\societyBot-master\quotes.txt", encoding='utf8') as f:
-       text = f.read()
+    with open(quotes_path, encoding='utf8') as quote_file:
+       text = quote_file.read()
 
     # Build the model.
-    text_model1 = markovify.NewlineText(text, state_size=2)
+    text_model = markovify.NewlineText(text, state_size=state_size)
     
+    return text_model.make_short_sentence(sequence_length)
 
-    return text_model1.make_short_sentence(50)
-
+def parse_sysargs(parser):
+    options, _ = parser.parse_args()
+    use_default = options.default
+    input_config = {"output_path": options.output_path,
+                    "dims": [int(n) for n in options.dims.split('x')],
+                    "seed": int(options.seed)}
+    config = load_default_config_()
+    if not use_default:
+        config.update(input_config)
+    with open("./config.json", "w") as config_file:
+        json.dump(config, config_file, indent=4)
+    return config_tuple(**config)
 
 def MakeBackground():
      # Parse command-line options
     parser = optparse.OptionParser()
-    parser.add_option('-o', '--output', dest='outputPath', help='Write output to FILE', metavar='FILE')
+    parser.add_option('-o', '--output', dest='output_path', default="./output.png", help='Write output to FILE', metavar='FILE')
     parser.add_option('-d', '--dims', dest='dims', default='512x512', help='Image width x height, e.g. 320x240')
     parser.add_option('-s', '--seed', dest='seed', default=int(1000 * time.time()), help='Random seed (uses system time by default)')
-    options, _ = parser.parse_args()
+    parser.add_option('-D', '--default_config', dest='default', default=False, action="store_true", help='Use default configuration file.')
     
-    dX, dY = (int(n) for n in options.dims.lower().split('x'))
-    #create the seed
-    try:
-        options.seed = int(options.seed)
-    except ValueError:
-        pass
-    random.seed(options.seed)
-    if not options.outputPath:
-        options.outputPath = r'C:\Users\Administrator\Desktop\societyBot-master\output.png'
+    config = parse_sysargs(parser)
+
+    
+    dX, dY = config.dims
+
+    random.seed(config.seed)
 
     # Generate x and y images, with 3D shape so operations will correctly broadcast.
-    xArray = np.linspace(0.0, 1.0, dX).reshape((1, dX, 1))
-    yArray = np.linspace(0.0, 1.0, dY).reshape((dY, 1, 1))
+    xArray = np.linspace(0., 1., dX).reshape(1, -1, 1)
+    yArray = np.linspace(0., 1., dY).reshape(-1, 1, 1)
 
     # Adaptor functions for the recursive generator
     # Note: using python's random module because numpy's doesn't handle seeds longer than 32 bits.
-    def randColor(): return np.array([random.random(), random.random(), random.random()]).reshape((1, 1, 3))
-    def xVar(): return xArray
-    def yVar(): return yArray
-    def safeDivide(a, b): return np.divide(a, np.maximum(b, 0.001))
+    randColor = lambda: np.random.rand(1, 1, 3)
+    x_var = lambda: xArray
+    y_var = lambda: yArray
+    def safe_divide(a, b, eps=1e-3):
+        b[np.abs(b) < eps] = np.sign(b[np.abs(b) < eps])*eps
+        b[b==0] = eps
+        return np.divide(a, b)
+
+    def get_random_function(depth=0):
+        functions = (
+                (0, randColor),
+                (0, x_var),
+                (0, y_var),
+                (1, np.sin),
+                (1, np.cos),
+                (2, np.add),
+                (2, np.subtract),
+                (2, np.multiply),
+                (2, safe_divide),
+            )
+        funcs = [(n_args, function) for (n_args, function) in functions if
+                    (n_args > 0 and depth < config.max_depth) or
+                    (n_args == 0 and depth >= config.min_depth)]
+        n_args, func = random.choice(funcs)
+        return n_args, func
 
     # Recursively build an image using a random function.  Functions are built as a parse tree top-down,
     # with each node chosen randomly from the following list.  The first element in each tuple is the
     # number of required recursive calls and the second element is the function to evaluate the result.
-    functions = (
-            (0, randColor),
-            (0, xVar),
-            (0, yVar),
-            (1, np.sin),
-            (1, np.cos),
-            (2, np.add),
-            (2, np.subtract),
-            (2, np.multiply),
-            (2, safeDivide),
-        )
-
-    depthMin = 5
-    depthMax = 15
-
-    def buildImg(depth = 0):
-        funcs = [f for f in functions if
-                    (f[0] > 0 and depth < depthMax) or
-                    (f[0] == 0 and depth >= depthMin)]
-        nArgs, func = random.choice(funcs)
-        args = [buildImg(depth + 1) for n in range(nArgs)]
+    def build_img(depth=0):
+        n_args, func = get_random_function(depth)
+        args = [build_img(depth + 1) for n in range(n_args)]
         return func(*args)
 
-    img = buildImg()
+
+    img = build_img()
 
     # Ensure it has the right dimensions
     img = np.tile(img, (dX / img.shape[0], dY / img.shape[1], 3 / img.shape[2]))
 
     # Convert to 8-bit, send to PIL and save
-    img8Bit = np.uint8(np.rint(img.clip(0.0, 1.0) * 255.0))
+    img_8bit = np.uint8(np.rint(img.clip(0.0, 1.0) * 255.0))
     
-    Image.fromarray(img8Bit).save(options.outputPath)
-    print('Seed %s; wrote output to %s' % (repr(options.seed), options.outputPath))
+    Image.fromarray(img_8bit).save(config.output_path)
+    print('Seed {}; wrote output to {}'.format(config.seed, config.output_path))
 
 
 # This is being worked on, as wrapped text won't draw an outline for some reason
@@ -110,21 +140,21 @@ def TextWrap(text, font, max_width):
         return lines
 
 
-def CombineImage(text):
+def CombineImage(text, temp_filepath="./output.png", output_filepath="./output_with_text.png", font_filepath="./zalgo.ttf"):
 
 
     #make the new image
-    img = Image.open(r"C:\Users\Administrator\Desktop\societyBot-master\output.png")
+    img = Image.open(temp_filepath)
     width, height = img.size
     #setup d for drawing
-    d = ImageDraw.Draw(img)
+    drawing = ImageDraw.Draw(img)
     #set image size
     image_size = (width, height)
     #set the colour of text & outline
-    textcolour = (0,0,0)
+    textcolour = (0, 0, 0)
     textOutline = (255, 255, 255)
     #create the font and set text size
-    fnt = ImageFont.truetype(r"C:\Users\Administrator\Desktop\societyBot-master\zalgo.ttf", 40, encoding="unic")
+    fnt = ImageFont.truetype(font_filepath, 40, encoding="unic")
 
     draw = ImageDraw.Draw(img)
     #Set outline amount, sets how many times we loop
@@ -158,61 +188,63 @@ def CombineImage(text):
     #check text's length to see if we need to wrap
     if len(text) >= 55:
 
-        
         WrappedText1 = TextWrap(text, fnt, image_size[0])
 
         for lines in WrappedText1:
 
             if lines == None:
                 break
-
             else:
                 #draw the lines on the image
-                d.text((x,y), lines, fill=textcolour, font=fnt)
-                
-                
-
+                drawing.text((x,y), lines, fill=textcolour, font=fnt)
                 y = y + line_height
     
     else:
-        d.text((x,y), text, font=fnt, fill=textcolour)
+        drawing.text((x,y), text, font=fnt, fill=textcolour)
 
 
-    img.save(r"C:\Users\Administrator\Desktop\societyBot-master\outputWithText.png", optimize=True)
+    img.save(output_filepath, optimize=True)
     print('Text written')
 
 
-def postToFacebook(post_text):
+def postToFacebook(post_text, filepath="./output_with_text.png", post=True):
     #obvs token is hidden
+    if not post:
+        return
     token = ""
     graph = facebook.GraphAPI(token)
-    post_id = graph.put_photo(image = open(r"C:\Users\Administrator\Desktop\societyBot-master\outputWithText.png", 'rb'), message=post_text)['post_id']
-    
+    with open(filepath, 'rb') as image_file:
+        post_id = graph.put_photo(image = image_file, message=post_text)['post_id']
     print(f"Success in uploading {post_id} to facebook")
 
-
 def job():
-    title = makeText()
+    config = load_default_config()
+    title = makeText(config.quotes_path,
+                     config.markov_model_state_size,
+                     config.sequence_length).lower()
     print(title)
     
+    combine_args = (config.output_background_path,
+                    config.output_path,
+                    config.font_path)
+    post_args = (config.output_path,
+                 config.post_to_facebook)
     # Try/Except is a basic way of keeping the bot up
     # while the image or text can fail to generate
-    try:
-        MakeBackground()
-        CombineImage(title.lower())
-        postToFacebook(title.lower())
+    # try:
+    MakeBackground()
+    CombineImage(title, *combine_args)
+    postToFacebook(title, *post_args)
 
-    except:
-        print("Backgrounf failed to generate")
-
-        CombineImage("failed")
-
-        postToFacebook("failed")
-
+    # except:
+    #     print("Background failed to generate")
+    #     CombineImage("failed", *combine_args)
+    #     postToFacebook("failed", *post_args)
 
 if __name__ == '__main__':
 
-    schedule.every(30).minutes.do(job).run()
+    delay = get_config("post_delay_minutes")
+    schedule.every(delay).minutes.do(job).run()
     
     while True:
         schedule.run_pending()
